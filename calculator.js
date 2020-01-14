6a74eeb68f1053af039fd424f7d4a8f770e46fa7
ingridolea <ingridolea@gmail.com>
2020-01-13 19:50:23 -0500

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function sumajs(a,b) { 
+    return a + b
+}
+
+function restajs(a,b) { 
+    return a - b
+}
+
+function multjs(a,b) { 
+    return a * b
+}
+
+function divjs(a,b) { 
+    return a / b
+}
