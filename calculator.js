e942d5f264ae438d49c692936e1cc3540a357e8a
ingridolea <ingridolea@gmail.com>
2020-01-20 21:38:05 -0500

    added comments for assingment submission

--- calculator.js
+++ calculator.js
@@ -1,19 +1,24 @@
+//Sum function
 function sumajs(a,b) { 
     return a + b
 }
 
+//Substraction function
 function restajs(a,b) { 
     return a - b
 }
 
+//Multiplication function
 function multjs(a,b) { 
     return a * b
 }
 
+//Division function
 function divjs(a,b) { 
     return a / b
 }
 
+//Trying in the console
 console.log("I'm going use the calculator functions to multiply 6 and 7")
 var x = multjs(6,7)
 console.log(x)
--- calculator.py
+++ calculator.py
@@ -1,28 +1,37 @@
+#Defining sum
 def suma(a,b):
 	return a+b
 
+#Defining substraction
 def resta(a,b):
 	return a-b
 
+#Defining multiplication
 def mult(a,b):
 	return a*b
 
+#Defining division
 def div(a,b):
 	return a/b
 
+#Defining special "square" function
 def square(a):
 	return a**2
 
+#Defining special "cube" function
 def cubo(a):
 	return a**3
 
+#Defining special "square_n_times" function
 def square_n_times(number,n):
 	return square(number)**n
 
+#Trying in the console
 print("I'm going use the calculator functions to multiply 5 and 6")
 x = mult(5,6)
 print(x)
 
+#Trying square n times in the console
 print("I'm going use the weird defined function square_n_times with values 9 and 6")
 x = square_n_times(9,6)
 print(x)
