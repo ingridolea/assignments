eaf7c464dd94972fb5ce6ab65da3ae69e3222e90
ingridolea <ingridolea@gmail.com>
2020-01-20 20:53:09 -0500

    Added some comments to the code to make it more understandable

--- add1.py
+++ add1.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+
+#imports from Standard Input
 import sys
 input_number = sys.stdin.read()
+
+#prints the input +1
 print(int(input_number) + 1)
--- aplusb.py
+++ aplusb.py
@@ -1,3 +1,5 @@
+#defines variables
 a=5
 b=7
+#prints out to console
 print("The sum of a and b is "+str(a+b))
